bda05cda12362366d06f4a45695d1930405caa7a
Jannis Limperg <jannis@limperg.de>
2015-04-11 18:16:07 +0200

    Automatic release generation
    This commit adds a simple script to generate release packages for Linux
    and Windows. Executing './mkrelease.sh' should now generate
    ready-for-upload binary packages in Packages/. See the usage note inside
    the script for details.
    
    In the process, the launch script for Linux is replaced by one that
    explicitly requests the 4.0 Mono runtime. This fixes a bug where the
    version autodetection failed under certain circumstances.
    
    fixes #2

---
+++ mkrelease.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# (c) 2015 Jannis Limperg
+# This software is licensed under the terms of the GNU General Public License
+# version 3, a copy of which can be found in the LICENSE file.
+
+# Usage: Generate binary packages by building the Packages project in
+# Monodevelop, then execute this script in the Git top directory. Packages for
+# the current version will be generated in Packages/. Make sure that the commit
+# you have currently checked out corresponds to the version you want to
+# generate.
+#
+# The version number is automatically determined by looking at the
+# current commit's annotated tag. The tag must be "va.b.c", where
+# a.b.c is the version number.
+
+VERSION_PATTERN='^v([0-9]+.[0-9]+.[0-9]+)$'
+PACKAGES_PROJECT_FILE='Packages.mdproj'
+PACKAGEDIR='Packages'
+LINUX_PACKAGE='opdtab_bin_linux-x86.tar'
+WINDOWS_PACKAGE='opdtab_bin_windows-x86.zip'
+LAUNCHSCRIPT='opdtab'
+
+function die() {
+  echo $1
+  exit 1
+}
+
+# Get version string from Git. As a safety measure, we only allow a release
+# to be created if the commit currently checked out is tagged with an
+# annotated tag.
+
+describe_string=$(git describe --exact-match)
+
+echo "$describe_string" | grep -Eq "$VERSION_PATTERN"
+if [ $? -ne 0 ]; then
+  die "Could not detect version. Do you have a tagged commit checked out?"
+fi
+
+version_string=$(echo "$describe_string" | sed -re "s/${VERSION_PATTERN}/\\1/")
+
+# Generate the binary packages.
+
+mdtool build "${PACKAGES_PROJECT_FILE}"
+
+# Add launch script to linux package and compress.
+
+tar -rC "${PACKAGEDIR}" -f "${PACKAGEDIR}/${LINUX_PACKAGE}" "${LAUNCHSCRIPT}" \
+  || die "Unable to add launchscript to linux tarball."
+bzip2 -9 "${PACKAGEDIR}/${LINUX_PACKAGE}" \
+  || die "Unable to compress linux package using bzip2."
+
+# Name tarballs properly.
+
+mv "${PACKAGEDIR}/${LINUX_PACKAGE}.bz2" \
+   "${PACKAGEDIR}/opdtab_bin_linux-x86_${version_string}.tar.bz2" \
+   || die "Unable to rename linux package."
+
+mv "${PACKAGEDIR}/${WINDOWS_PACKAGE}" \
+   "${PACKAGEDIR}/opdtab_bin_windows-x86_${version_string}.zip" \
+   || die "Unable to rename windows package."
